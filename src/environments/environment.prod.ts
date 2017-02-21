f27fbf7c349022ae85463d4d3958da271226c923
gloriatse <gloriatse@192.168.0.102>
2017-02-22 07:26:36 +0800

    add production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,8 @@ export const environment = {
   api_fund_type_action:'/rest/action?requestAction=',
   api_fund_type_stat: '/rest/stat?asOfDate=',
   api_fund_detail: '/rest/details',
-  api_fund_type_filter: '/rest/mpffilter'
-
+  api_fund_type_filter: '/rest/mpffilter',
+  api_rss_to_json: 'https://rss2json.com/api.json?rss_url=',
+  rss_mpfa_press:'http://www.mpfa.org.hk/eng/main/rss/xml/press_rss.xml'
+  
 };
